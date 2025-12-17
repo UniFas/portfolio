f41200687de3d01ab9d07fa2dfab30665f121a8d
Francisco Santos <unifasdraws@gmail.com>
2025-12-17 10:36:02 +0000

    Update portfolio

---
+++ js/menu.js
@@ -0,0 +1,17 @@
+
+//"use strict";
+let menuIcon=document.querySelector("#menuIcon");
+
+menuIcon.onclick=function(){
+    showMenu(this);
+};
+
+ function showMenu(x){
+ x.classList.toggle("change");
+ let menu=document.querySelector("#navbar");
+if (menu.style.maxHeight && menu.style.maxHeight !== "0px") {
+  menu.style.maxHeight = "0px";
+} else {
+  menu.style.maxHeight = "210px";
+}
+ }
